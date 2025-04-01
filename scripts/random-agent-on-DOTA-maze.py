1630032994b69607fd86ac2f370e868e26268771
mo <cmoxiv@gmail.com>
2025-04-02 01:10:28 +1100

    Demos.

---
+++ scripts/random-agent-on-DOTA-maze.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from zeit4150envs.MazeEnv import MazeEnv
+
+env = MazeEnv()
+state, _ = env.reset()
+
+try:
+    while True:
+        action = env.action_space.sample()
+        state, reward, *done, info = env.step(action)
+
+        if any(done):
+            env.reset()
+
+except KeyboardInterrupt:
+    print("INTERRUPTED BY USER!!")
+finally:
+    env.close()
